abcdc5b2ec474e35fcbac7b021da07f7f2487b7b
Andriel Jose <andrieldata@proton.me>
2024-07-24 20:32:23 -0300

    Add: modified header and add Nav.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,34 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+import Nav from "./Nav";
+
 type Props = {};
 
 const Header = (props: Props) => {
-  return <header>Header</header>;
+  return (
+    <header className="py-8 xl:py-12 text-white">
+      <div className="container mx-auto flex justify-between items-center">
+        {/* Logo */}
+
+        <Link href="/">
+          <h1 className="text-4xl font-semibold">
+            ANDRIEL <span className="text-accent">.</span>
+          </h1>
+        </Link>
+
+        {/* desktop nav & contato*/}
+        <div className="hidden xl:flex items-center gap-8">
+          <Nav />
+          <Link href="/contato">
+            <Button>Contato</Button>
+          </Link>
+        </div>
+
+        {/* mobile nav */}
+        <div className="xl:hidden">mobile nav</div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
