527251675d075990895d870e60d412e405bd78a6
Andriel Jose <andrieldata@proton.me>
2024-07-29 14:57:03 -0300

    fix delay in education

--- src/app/curriculo/page.tsx
+++ src/app/curriculo/page.tsx
@@ -114,7 +114,7 @@ const Curriculo = () => {
 
             {/* Education */}
             <TabsContent value="education" className="w-full">
-              <FramerWrapper y={0} scale={0.8} delay={4} duration={0.15}>
+              <FramerWrapper>
                 <div className="flex flex-col gap-[30px] text-center xl:text-left">
                   <h3 className="text-4xl font-bold">{Education.title}</h3>
                   <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0 whitespace-pre-line">
