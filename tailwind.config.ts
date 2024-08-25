54548a985d80c0a6f376f2c4d40327a40f3a09e1
andriel300 <andrieldata@proton.me>
2024-08-24 21:07:26 -0300

    double quote prettier

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,71 +1,71 @@
-import type { Config } from 'tailwindcss';
-import plugin from 'tailwindcss/plugin';
-import type { PluginAPI } from 'tailwindcss/types/config';
+import type { Config } from "tailwindcss";
+import plugin from "tailwindcss/plugin";
+import type { PluginAPI } from "tailwindcss/types/config";
 
 const config: Config = {
-  darkMode: ['class'],
+  darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
-  prefix: '',
+  prefix: "",
   theme: {
     container: {
       center: true,
-      padding: '1rem',
+      padding: "1rem",
     },
     screens: {
-      xs: '480px',
-      sm: '640px',
-      md: '768px',
-      lg: '960px',
-      xl: '1200px',
+      xs: "480px",
+      sm: "640px",
+      md: "768px",
+      lg: "960px",
+      xl: "1200px",
     },
     fontFamily: {
-      primary: 'var(--font-jetbrainsMono)',
+      primary: "var(--font-jetbrainsMono)",
     },
     extend: {
       textShadow: {
-        sm: '0 1px 2px var(--tw-shadow-color)',
-        DEFAULT: '0 2px 4px var(--tw-shadow-color)',
-        lg: '0 8px 16px var(--tw-shadow-color)',
+        sm: "0 1px 2px var(--tw-shadow-color)",
+        DEFAULT: "0 2px 4px var(--tw-shadow-color)",
+        lg: "0 8px 16px var(--tw-shadow-color)",
       },
       colors: {
-        primary: '#16161a',
+        primary: "#16161a",
         accent: {
-          DEFAULT: '#7f5af0',
-          hover: '#8B6AF1',
+          DEFAULT: "#7f5af0",
+          hover: "#8B6AF1",
         },
-        accent2: '#2CAF86',
+        accent2: "#2CAF86",
       },
       keyframes: {
-        'accordion-down': {
-          from: { height: '0' },
-          to: { height: 'var(--radix-accordion-content-height)' },
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
         },
-        'accordion-up': {
-          from: { height: 'var(--radix-accordion-content-height)' },
-          to: { height: '0' },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
         },
       },
       animation: {
-        'accordion-down': 'accordion-down 0.2s ease-out',
-        'accordion-up': 'accordion-up 0.2s ease-out',
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
   plugins: [
-    require('tailwindcss-animate'),
+    require("tailwindcss-animate"),
     plugin(function ({ matchUtilities, theme }: PluginAPI) {
       matchUtilities(
         {
-          'text-shadow': (value: string) => ({
+          "text-shadow": (value: string) => ({
             textShadow: value,
           }),
         },
-        { values: theme('textShadow') },
+        { values: theme("textShadow") },
       );
     }),
   ],
